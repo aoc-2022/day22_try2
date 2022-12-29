35ab496978dda8b0e2c59acd5360a4ba7c13fa2c
xeno <xeno@lv427.space>
2022-12-29 14:03:12 +0100

    implemented movement, but likely completely wrong

--- Walker.fs
+++ Walker.fs
@@ -3,6 +3,8 @@ module day22_try2.Walker
 open day22_try2.Cube
 open day22_try2.BaseTypes
 
+exception NotImplemented of string
+
 type WalkState(cube: Cube, commands: Command list, location: Location, pos: RPos, facing: Direction) =
     member this.Cube = cube
     member this.Commands = commands
@@ -11,7 +13,7 @@ type WalkState(cube: Cube, commands: Command list, location: Location, pos: RPos
     member this.Pos = pos
 
     override this.ToString() =
-        let nextCommand = if commands.IsEmpty then "[]" else $"{commands.Head}" 
+        let nextCommand = if commands.IsEmpty then "[]" else $"{commands.Head}"
         $"WalkState: at {location} {pos} facing: {facing} - nextCommand: {nextCommand}"
 
 let turnRight (direction: Direction) =
@@ -27,21 +29,39 @@ let turnLeft (direction: Direction) =
     | North -> West
     | West -> South
     | South -> East
-    
-let nextLocation (location:Location) (dir:Direction) : Location*Direction =
-    match location,dir with
-    | Front,North -> Top,South 
-    | Front,East -> Right,West 
-    | Front,West -> Left,East
-    | Front,South -> Bottom,South
-    | Top,South -> Front,North
-    | Top,West -> Left,North
-    | Top,East -> Right,North
-    | Top,North -> Back,North
-    // rest is not implemented yet 
+
+let nextLocation (location: Location) (dir: Direction) : Location * Direction =
+    match location, dir with
+    | Front, North -> Top, South
+    | Front, East -> Right, West
+    | Front, West -> Left, East
+    | Front, South -> Bottom, South
+    | Top, South -> Front, North
+    | Top, West -> Left, North
+    | Top, East -> Right, North
+    | Top, North -> Back, North
+    | Bottom, North -> Back, South
+    | Bottom, South -> Front, South
+    | Bottom, West -> Left, South
+    | Bottom, East -> Right, South
+    | Back, North -> Top, North
+    | Back, South -> Bottom, South
+    | Back, West -> Left, East
+    | Back, East -> Right, East
+    | Left, North -> Top, West
+    | Left, South -> Bottom, West
+    | Left, West -> Front, West
+    | Left, East -> Back, East
+    | Right, North -> Top, East
+    | Right, South -> Bottom, East
+    | Right, West -> Front, East
+    | Right, East -> Back, East
+
+    | _ -> raise (NotImplemented $"{location},{dir}")
+// rest is not implemented yet
 
 let nextStepWithinBounds (state: WalkState) =
-    let last = state.Cube.SideLength-1
+    let last = state.Cube.SideLength - 1
 
     match state.Pos with
     | 0, _ when state.Facing = West -> false
@@ -57,41 +77,43 @@ let nextStep (state: WalkState) : RPos =
     | (x, y), North -> (x, y - 1)
     | (x, y), South -> (x, y + 1)
 
-let getEdgePos ((x,y):RPos) (dir:Direction) =
+let getEdgePos ((x, y): RPos) (dir: Direction) =
     match dir with
     | North -> x
     | South -> x
     | East -> y
     | West -> y
 
-let incomingPos (size:int) (edgePos:int) (inDir:Direction) ((n,e):NorthEast) =
+let incomingPosDir (size: int) (edgePos: int) (inDir: Direction) ((n, e): NorthEast) : Direction * RPos =
     let realDir =
         match inDir with
         | North -> n
         | South -> n |> oppositeDirection
         | East -> e
         | West -> e |> oppositeDirection
+
     let posDir =
         match inDir with
         | North -> e
         | South -> e
         | East -> n
         | West -> n
-    match realDir,posDir with // this is all wrong
-        | North,East -> (edgePos,0)
-        | North,West -> (size - 1 - edgePos,0)
-        | South,East -> (edgePos,size-1)
-        | South,West -> (size - 1 - edgePos,size - 1)
-        | East,North -> (0,size - 1 - edgePos)
-        | East,South -> (0,edgePos)
-        | West,North -> (size - 1,size - 1 - edgePos)
-        
-        
-        
-        
 
-let nextRPos (pos:RPos) (fromDir:Direction) (toDir:Direction) (toNE:NorthEast) = 1
-    
+    let pos =
+        match realDir, posDir with // this is likely all wrong
+        | North, East -> (edgePos, 0)
+        | North, West -> (size - 1 - edgePos, 0)
+        | South, East -> (edgePos, size - 1)
+        | South, West -> (size - 1 - edgePos, size - 1)
+        | East, North -> (size - 1, size - 1 - edgePos)
+        | East, South -> (size - 1, edgePos)
+        | West, North -> (0, size - 1 - edgePos)
+        | West, South -> (0, edgePos)
+
+    realDir, pos
+
+let nextRPos (pos: RPos) (fromDir: Direction) (toDir: Direction) (toNE: NorthEast) = 1
+
 
 let private step (state: WalkState) =
     match state.Commands with
@@ -100,25 +122,34 @@ let private step (state: WalkState) =
     | Steps 0 :: rest -> WalkState(state.Cube, rest, state.Location, state.Pos, state.Facing)
     | Steps n :: rest ->
         if nextStepWithinBounds state then
-            let next : RPos = nextStep state
+            let next: RPos = nextStep state
             printfn $"safe: {state} step {n} -> {next}"
-            if state.Cube.Sides[state.Location].Tiles.Contains next then
-                WalkState(state.Cube, Steps(n-1)::rest,state.Location,next,state.Facing)
+
+            if state.Cube.Sides[ state.Location ].Tiles.Contains next then
+                WalkState(state.Cube, Steps(n - 1) :: rest, state.Location, next, state.Facing)
             else
-               printfn $"blocker at {state.Pos} throwing away Steps {n}"
-               WalkState(state.Cube, rest, state.Location, state.Pos, state.Facing)  
+                printfn $"blocker at {state.Pos} throwing away Steps {n}"
+                WalkState(state.Cube, rest, state.Location, state.Pos, state.Facing)
         else
-            let (newLoc,newDir) = nextLocation state.Location state.Facing
-            printfn $"unsafe: {state} next={newLoc},{newDir}"
-            state
+            let (newLoc, newDir) = nextLocation state.Location state.Facing
+            let edgePos = getEdgePos state.Pos state.Facing
+
+            let (newDir, newPos) =
+                incomingPosDir state.Cube.SideLength edgePos newDir (state.Cube.Sides[newLoc].NE)
+
+            if state.Cube.Sides[ newLoc ].Tiles.Contains newPos then
+                WalkState(state.Cube, Steps(n - 1) :: rest, newLoc, newPos, newDir)
+            else
+                printfn $"Blocked when trying to enter {newLoc} from {state}"
+                WalkState(state.Cube, rest, state.Location, state.Pos, state.Facing)
 
 let walkAlongCube (cube: Cube) (commands: Command list) =
     let state = WalkState(cube, commands, Front, (0, 0), East)
-    let rec nsteps (n:int) (state:WalkState) =
-        if n = 0 then state
-        else
-            step state |> nsteps (n - 1)
+
+    let rec nsteps (n: int) (state: WalkState) =
+        if n = 0 then state else step state |> nsteps (n - 1)
+
     printfn $"State(1) {state}"
-    let state = nsteps 18 state 
+    let state = nsteps 18 state
     printfn $"State({8}) {state}"
     1
